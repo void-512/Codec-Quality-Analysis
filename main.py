7fbb47c84906adc5ac1896ecda33b2ea4a3e909c
Tony Wang <wty@WTYs-macbook-air.local>
2024-10-25 17:33:13 +0800

    changed name to 'log generation.py'

--- main.py
+++
@@ -1,7 +0,0 @@
-import generatetarget
-import readandstore
-import psnr
-
-dfVideo, dfCodec, dfBitrate = readandstore.generateConfigDF('exampleinput.txt')
-df = generatetarget.videosGenerator(dfVideo['Full Name'], dfCodec['Codec'], dfBitrate['Bitrate'])
-psnr.generateLogs(df)
